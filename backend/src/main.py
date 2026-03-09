f99ad7de3a257ae7738baa455196fec2040f7aa0
orioha1 <orioha@post.bgu.ac.il>
2026-03-09 13:27:33 +0200

    Add Dockerfile and dockerignore for production deployment

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.infrastructure.db.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.db import models
 from src.api.routes import auth_routes, management_routes, stock_routes
 
@@ -7,6 +8,14 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="StockUp API", version="1.0.0")
 
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 app.include_router(auth_routes.router)
 app.include_router(management_routes.router)
 app.include_router(stock_routes.router)
