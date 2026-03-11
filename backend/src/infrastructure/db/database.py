9af2e33c102a9fed0555b9d99bace64c9c1749e7
davidelbaz1 <85520776+davidelbaz1@users.noreply.github.com>
2026-03-11 13:33:30 +0200

    installing alembic for imigrations actions

--- backend/src/infrastructure/db/database.py
+++ backend/src/infrastructure/db/database.py
@@ -3,10 +3,14 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()  # Load environment variables from .env file
 
+load_dotenv()  
+load_dotenv("backend/.env")
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5433/stockup_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
